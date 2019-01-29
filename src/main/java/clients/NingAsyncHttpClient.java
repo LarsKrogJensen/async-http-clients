fe78fe33bc30ae6eb18fa23d8040c80d4b6e5613
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2019-01-29 18:04:41 +0100

    coding

--- src/main/java/clients/NingAsyncHttpClient.java
+++ src/main/java/clients/NingAsyncHttpClient.java
@@ -1,10 +1,16 @@
 package clients;
 
+import ch.qos.logback.core.util.CloseUtil;
+import com.spotify.futures.CompletableFutures;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.Dsl;
 import org.asynchttpclient.Response;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static clients.KazbiAsyncHttpClient.fromKazbiClient;
 import static clients.KazbiAsyncHttpClient.newKazbiClient;
@@ -13,27 +19,44 @@ import static org.asynchttpclient.Dsl.config;
 
 public class NingAsyncHttpClient {
     public static void main(String[] args) {
-        var config = config();
+        var config = config().setThreadPoolName("MYCLIENT");
 
         // setup once
         AsyncHttpClient asyncClient = asyncHttpClient(config);
         var template = newKazbiClient()
                 .withAsyncClient(asyncClient)
                 .withRetryOptions(new RetryOptions(10, 1_000))
-                .withServiceLookup(() -> Optional.of("http://httpbinm.org"))
+                .withServiceLookup(() -> Optional.of("http://httpbin.org"))
                 .withCircuitBreakerFactory(baseUrl -> new CircuitBreakerImpl())
                 .build();
 
         // Per request
-        fromKazbiClient(template)
-                .withRequestFactory(baseUrl -> Dsl.get(baseUrl + "/delay/1").setReadTimeout(5_000))
+        List<CompletableFuture<String>> futures = IntStream.range(0, 100).mapToObj(id -> runRequest(template, id)).collect(Collectors.toList());
+
+        CompletableFutures.allAsList(futures)
+                .whenComplete((response, error) -> {
+                    System.out.println("Done on thread " + Thread.currentThread().getName());
+                    CloseUtil.closeQuietly(asyncClient);
+                });
+
+
+//        CompletableFutures.exceptionallyCompose()
+    }
+
+    private static CompletableFuture<String> runRequest(KazbiAsyncHttpClient template, int id) {
+        return fromKazbiClient(template)
+                .withRequestFactory(baseUrl -> Dsl.get(baseUrl + "/delay/1"))
                 .execute()
                 .thenApply(Response::getResponseBody)
                 .whenComplete((result, error) -> {
                     if (error != null) {
                         System.out.println("ERROR: " + error.getMessage());
                     } else {
-                        System.out.println("OK: " + result);
+                        System.out.println(id + " - OK on thread " + Thread.currentThread().getName());
+//                        try {
+//                            Thread.sleep(1000);
+//                        } catch (InterruptedException e) {
+//                        }
                     }
                 });
     }
